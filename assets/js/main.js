d529921ee1b1918967af5ac6c32079247dc51607
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-02 17:55:03 +0100

    Creazione array

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// Chiedere mail utente
+var mail = prompt("Inserisci la tua mail");
+
+//console.log(mail);
+
+//Controlla che sia nella lista di chi può accedere
+
+  // Creo array per maining list
+
+var mainingList = ["prova@gmail.com", "nicolo@libero.it", "alessandro@yahoo.com", "fabrizio@live.it", "raffaele@alice.it"]
+
+  console.log(mainingList);
+
+  // 
+
+//Stampa un messaggio appropriato sull'esito del controllo
